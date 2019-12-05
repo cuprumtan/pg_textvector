cb8299fa80596ad8f5bf3c01b08d5a514b9bfe01
cuprumtan <cuprumtan@gmail.com>
2019-12-05 15:39:30 +0500

    Добавлено тестирование разных хешей

--- pg_textvector--0.0.1.sql
+++ pg_textvector--0.0.1.sql
@@ -1,3 +1,11 @@
-CREATE FUNCTION get_vector(integer, text)
-RETURNS float8[] AS 'pg_textvector', 'get_vector'
+CREATE FUNCTION get_vector_multihash(integer, text)
+RETURNS float8[] AS 'pg_textvector', 'get_vector_multihash'
+LANGUAGE C STRICT IMMUTABLE;
+
+CREATE FUNCTION get_vector_FNV1a(integer, text)
+RETURNS float8[] AS 'pg_textvector', 'get_vector_FNV1a'
+LANGUAGE C STRICT IMMUTABLE;
+
+CREATE FUNCTION get_vector_CRC8(integer, text)
+RETURNS float8[] AS 'pg_textvector', 'get_vector_CRC8'
 LANGUAGE C STRICT IMMUTABLE;
