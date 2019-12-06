76ace30568724787218c85cf0242e0967fc99ff3
cuprumtan <cuprumtan@gmail.com>
2019-12-06 13:47:38 +0500

    FNV1a до сих пор показывает лучшие результаты, удаление ненужных хешей

--- pg_textvector--0.0.1.sql
+++ pg_textvector--0.0.1.sql
@@ -1,15 +1,3 @@
-CREATE FUNCTION get_vector_multihash(integer, text)
-RETURNS float8[] AS 'pg_textvector', 'get_vector_multihash'
-LANGUAGE C STRICT IMMUTABLE;
-
-CREATE FUNCTION get_vector_FNV1a(integer, text)
-RETURNS float8[] AS 'pg_textvector', 'get_vector_FNV1a'
-LANGUAGE C STRICT IMMUTABLE;
-
-CREATE FUNCTION get_vector_CRC8(integer, text)
-RETURNS float8[] AS 'pg_textvector', 'get_vector_CRC8'
-LANGUAGE C STRICT IMMUTABLE;
-
-CREATE FUNCTION get_vector_float(integer, text)
-RETURNS float8[] AS 'pg_textvector', 'get_vector_float'
+CREATE FUNCTION get_vector(integer, text)
+RETURNS float8[] AS 'pg_textvector', 'get_vector'
 LANGUAGE C STRICT IMMUTABLE;
